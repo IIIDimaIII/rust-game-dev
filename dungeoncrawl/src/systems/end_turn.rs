11be6c85eaafa8d66eb3b4a7fa40592389fcbe73
Dima <dterenin@gmail.com>
2024-02-14 17:50:22 +0200

    Add map level to Player struct and implement level advancement

--- dungeoncrawl/src/systems/end_turn.rs
+++ dungeoncrawl/src/systems/end_turn.rs
@@ -5,7 +5,12 @@ use crate::prelude::*;
 #[read_component(Player)]
 #[read_component(Point)]
 #[read_component(AmuletOfYala)]
-pub fn end_turn(ecs: &SubWorld, #[resource] turn_state: &mut TurnState) {
+pub fn end_turn(
+    ecs: &SubWorld, 
+    #[resource] turn_state: &mut TurnState,
+    #[resource] map: &Map,
+) {
+    let amulet_default = Point::new(-1, -1);
     let mut player_hp = <(&Health, &Point)>::query().filter(component::<Player>());
     let mut amulet = <&Point>::query().filter(component::<AmuletOfYala>());
     let current_state = *turn_state;
@@ -15,14 +20,18 @@ pub fn end_turn(ecs: &SubWorld, #[resource] turn_state: &mut TurnState) {
         TurnState::MonsterTurn => TurnState::AwaitingInput,
         _ => current_state
     };
-    // let amulet_pos = amulet.iter(ecs).nth(0).unwrap();
-    let amulet_pos = amulet.iter(ecs).next().unwrap();
+    let amulet_pos = amulet.iter(ecs).nth(0).unwrap_or(&amulet_default);
+    // let amulet_pos = amulet.iter(ecs).next().unwrap();
     player_hp.iter(ecs).for_each(|(hp, pos)| {
         if hp.current < 1{
             new_state = TurnState::GameOver;
         }
         if pos == amulet_pos {
             new_state = TurnState::Victory;
+        }
+        let idx = map.point2d_to_index(*pos);
+        if map.tiles[idx] == TileType::Exit {
+            new_state = TurnState::NextLevel;
         }
     });
     *turn_state = new_state;
